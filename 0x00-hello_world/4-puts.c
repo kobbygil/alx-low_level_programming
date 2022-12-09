98a9c8b3012bb7ab94ce06de704cce9827b969e7
kobbygil <kobbygil@yahoo.com>
2022-12-08 21:35:24 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * return - to return 0
+ *
+ * return :always 0 (sucess)
  */
+
 int main(void)
 {
-	puts("\""Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
